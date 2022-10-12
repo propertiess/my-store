7f99ccc667b0884237257152a65ed7f82b9326dd
propertiess <erofeev2001@yandex.ru>
2022-10-12 19:32:56 +0300

    (@hooks: useClassName, useModal): Created tests

---
+++ app/hooks/tests/useClassName.test.ts
@@ -0,0 +1,31 @@
+import '@testing-library/jest-dom';
+import { act, cleanup, renderHook } from '@testing-library/react';
+import { useClassName } from '@hooks/useClassName';
+
+afterEach(() => {
+  cleanup();
+});
+
+describe('ClassName', () => {
+  test('default state is correct', () => {
+    const { result } = renderHook(() => useClassName('mainClass', 'addClass'));
+    expect(result.current.currClassName).toBe('mainClass');
+  });
+
+  test('toggleClassList works correctly', () => {
+    const { result } = renderHook(() => useClassName('mainClass', 'addClass'));
+
+    act(() => {
+      result.current.toggle();
+    });
+    expect(result.current.currClassName).toBe('mainClass addClass');
+
+    for (let i = 0; i < 2; i++) {
+      act(() => {
+        result.current.toggle();
+      });
+    }
+
+    expect(result.current.currClassName).toBe('mainClass addClass');
+  });
+});
---
+++ app/hooks/tests/useModal.test.ts
@@ -0,0 +1,28 @@
+import '@testing-library/jest-dom';
+import { act, cleanup, renderHook } from '@testing-library/react';
+import { useModal } from '@hooks/useModal';
+
+afterEach(() => cleanup());
+
+describe('Modal', () => {
+  test('isShow equal initialState', () => {
+    const { result } = renderHook(() => useModal(false));
+    expect(result.current.isShow).toBe(false);
+  });
+
+  test('toggle works correctly', () => {
+    const { result } = renderHook(() => useModal(false));
+
+    act(() => {
+      result.current.toggleShow();
+    });
+    expect(result.current.isShow).toBe(true);
+
+    for (let i = 0; i < 3; i++) {
+      act(() => {
+        result.current.toggleShow();
+      });
+    }
+    expect(result.current.isShow).toBe(false);
+  });
+});
